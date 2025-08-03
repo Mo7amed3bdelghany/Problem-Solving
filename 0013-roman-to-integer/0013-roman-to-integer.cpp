a08e9897ecae4f17c61568cf63e97a00ef51a7e7
Mohamed Abdelghany <mohamed.abdelghany726@gmail.com>
2025-08-03 16:52:08 +0300

    Time: 5 ms (59.85%), Space: 13.5 MB (9.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,26 +5,11 @@ public:
         mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; mp['C']=100; mp['D']=500; mp['M']=1000;
         int ans=0,check=1;
         for (int i=0;i<size(s)-1;i++){
-            if(s[i]=='I' && (s[i+1]=='V'||s[i+1]=='X')){
-                ans+=mp[s[i+1]]-mp[s[i]];
-                i++;
-                if(i==size(s)-1) check=0;
-            }
-            else if(s[i]=='X' && (s[i+1]=='L'||s[i+1]=='C')){
-                ans+=mp[s[i+1]]-mp[s[i]];
-                i++;
-                if(i==size(s)-1) check=0;
-            }
-            else if(s[i]=='C' && (s[i+1]=='D'||s[i+1]=='M')){
-                ans+=mp[s[i+1]]-mp[s[i]];
-                i++;
-                if(i==size(s)-1) check=0;
-            }
+            if(mp[s[i]] < mp[s[i+1]]) ans-=mp[s[i]];
             else{
                 ans+=mp[s[i]];
             }
         }
-        if(check) ans+=mp[s[size(s)-1]];
-        return ans;
+        return ans + mp[s[size(s)-1]];
     }
 };
